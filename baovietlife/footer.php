bb9c1f72b7b79b18deb7d4b3796da1a20c623ff5
hprobotic <hprobotic@gmail.com>
2015-07-09 15:48:21 +0700

    Add header and footer php file

---
+++ baovietlife/footer.php
@@ -0,0 +1,54 @@
+<div class="section" id="section-footer">
+  <div id="footer-text">
+    <div class="container fluid posr clearfix">
+      <div class="row">
+        <div class="footerx">
+
+          <div class="grid7">
+            <div class="branding-logo clearfix">
+              <img src="images/brand-logo-w.png" alt="">
+              <ul class="list-connect">
+                <li>
+                  <i class="fa fa-facebook"></i>
+                </li>
+                <li>
+                  <i class="fa fa-twitter"></i>
+                </li>
+                <li>
+                  <i class="fa fa-google-plus"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="ft-menu clearfix">
+              <ul class="list-inline">
+                <li>sản phẩm</li>
+                <li>tin tức</li>
+                <li>lập kế hoạch hưu trí</li>
+                <li>© 2015 Bản quyền thuộc về  bảo hiểm bảo việt</li>
+              </ul>
+            </div>
+          </div>
+          <div class="grid5">
+            <div class="quick-link">
+              <div class="add-link">
+                <a href="/" class="chtg"></a>
+              </div>
+              <div class="sup-link">
+                <a href="/" class="sup-email">Email</a>
+                <a href="/" class="sup-hot">Hotline</a>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<script type='text/javascript' src='wp-includes/js/jquery/ui/core.min.js?ver=1.11.4'></script>
+<script type='text/javascript' src='wp-includes/js/jquery/ui/datepicker.min.js?ver=1.11.4'></script>
+</body>
+
+</html>
---
+++ baovietlife/header.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="lang=" en-US "" xml:lang="lang=" en-US "">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	<meta name="author" content="Time Universal" />
+	<meta name="description" content="" />
+	<meta property="fb:app_id" content="" />
+	<meta property="og:type" content='article' />
+	<meta property="og:title" content="PreIQ" />
+	<meta property="og:url" content="" />
+	<meta property="og:image" content="screenshot.png" />
+	<meta property="og:description" content="" />
+
+	<title>Bảo Việt Nhân Thọ</title>
+	<link type="image/x-icon" rel="shortcut icon" href="images/favicon.png">
+
+	<meta name='robots' content='noindex,follow' />
+
+	<style type="text/css">
+		img.wp-smiley,
+		img.emoji {
+			display: inline !important;
+			border: none !important;
+			box-shadow: none !important;
+			height: 1em !important;
+			width: 1em !important;
+			margin: 0 .07em !important;
+			vertical-align: -0.1em !important;
+			background: none !important;
+			padding: 0 !important;
+		}
+	</style>
+	<link rel='stylesheet' id='addons-css' href='addons.css?ver=4.2.2' type='text/css' media='all' />
+	<link rel='stylesheet' id='jquery-datepicker-css' href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css?ver=4.2.2' type='text/css' media='all' />
+	<link rel='stylesheet' id='style-css' href='style.css?ver=4.2.2' type='text/css' media='all' />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+	<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js?ver=1.9.1'></script>
+	<script type='text/javascript'>
+
+	</script>
+	<script type='text/javascript' src='scripts/addons.js?ver=1.0'></script>
+	<script type="text/javascript" src="http://stephband.info/jparallax/js/jquery.parallax.min.js"></script>
+	<script type='text/javascript' src='scripts/main.js?ver=1.0'></script>
+	<script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false&#038;ver=3.0'></script>
+</head>
+
+<body class="home blog">
+	<div id="header">
+		<div class="container fluid clearfix">
+			<div class="logo fl">
+				<a href="">
+					<img src="images/nav-logo.png" alt="Seabone">
+				</a>
+			</div>
+
+			<div class="menu fl">
+				<div id="hamburger"></div>
+				<ul class="nav lsn clearfix">
+					<div class="nav-close"></div>
+					<li id="menu-item-50" class="">
+						<a href="/" class="tac fwsb posr ttu tdn">
+							<span class="nav-icon">
+								<img src="images/icon-1.png" alt="">
+							</span>
+							<span class="nav-text">Trang chủ</span>
+						</a>
+					</li>
+					<li id="menu-item-57" class="">
+						<a href="san-pham-/" class="tac fwsb posr ttu tdn">
+							<span class="nav-icon">
+								<img src="images/icon-2.png" alt="">
+							</span>
+							<span class="nav-text">Sản phẩm</span>
+						</a>
+
+					</li>
+					<li id="menu-item-51" class="">
+						<a href="tintuc.php" class="tac fwsb posr ttu tdn">
+							<span class="nav-icon">
+								<img src="images/icon-3.png" alt="">
+							</span>
+							<span class="nav-text">Tin tức</span>
+						</a>
+
+					</li>
+					<li id="menu-item-53" class="">
+						<a href="lapkehoachhuutri.php" class="tac fwsb posr ttu tdn">
+							<span class="nav-icon">
+								<img src="images/icon-4.png" alt="">
+							</span>
+							<span class="nav-text">lập kế hoạch hưu trí</span>
+						</a>
+
+					</li>
+					<li id="menu-item-55" class="">
+						<a href="dichvukhachhang.php" class="tac fwsb posr ttu tdn">
+							<span class="nav-icon">
+								<img src="images/icon-5.png" alt="">
+							</span>
+							<span class="nav-text">dịch vụ khách hàng</span>
+						</a>
+
+					</li>
+					<li id="menu-item-55" class="">
+						<a href="lienhe.php" class="tac fwsb posr ttu tdn">
+							<span class="nav-icon">
+								<img src="images/icon-6.png" alt="">
+
+							</span>
+							<span class="nav-text">LIên hệ</span>
+						</a>
+
+					</li>
+				</ul>
+			</div>
+			<div class="nav-meta fr">
+				<div class="search fl clearfix">
+					<form action="/">
+						<input type="text" class="search-input" placeholder="">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
