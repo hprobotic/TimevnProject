866af4585616ddb8806cea6054a864f6c9715a82
hprobotic <hprobotic@gmail.com>
2015-07-10 11:05:05 +0700

    Add Product Detail
    Add Product Detail

---
+++ baovietlife/sanpham.php
@@ -0,0 +1,132 @@
+<?php include 'header.php'; ?>
+<style>
+  body {
+    overflow-y: scroll !important;
+  }
+</style>
+<div class="main-page"  id="sanpham">
+  <div class="page-cover">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="cover-info">
+            <h1>an hưu phát nghiệp</h1>
+            <p>
+              Giải pháp phuc lợi thu hút nhân tài
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="page-content">
+    <div class="s-content" id="page-title">
+      <div class="container">
+        <div class="row">
+          <div class="p-heading clearfix">
+            <div class="col-md-12 ">
+              <h1 class="p-title">an hưu phát nghiệp</h1>
+              <p class="p-tagline">Phúc lợi tốt, gắn kết bền</p>
+              <p class="p-hr"></p>
+              <p class="p-intro">Doanh nghiệp của Bạn liệu có đang phải đối mặt với các tình huống khó khăn như</p>
+            </div>
+          </div>
+          <div class="list-featured clearfix">
+            <div class="col-md-4">
+              <div class="ft-img">
+                <img src="images/featured1.png" alt="">
+              </div>
+              <div class="ft-info">
+                <p>Các nhân viên chưa thực sự say mê cống hiến và muốn gắn bó lâu dài với doanh nghiệp?</p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="ft-img">
+                <img src="images/featured1.png" alt="">
+              </div>
+              <div class="ft-info">
+                <p>Các nhân viên chưa thực sự say mê cống hiến và muốn gắn bó lâu dài với doanh nghiệp?</p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="ft-img">
+                <img src="images/featured1.png" alt="">
+              </div>
+              <div class="ft-info">
+                <p>Các nhân viên chưa thực sự say mê cống hiến và muốn gắn bó lâu dài với doanh nghiệp?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="s-content" id="why-us">
+      <div class="container">
+        <div class="row">
+          <div class="clearfix">
+            <div class="col-md-12">
+            <h2>Tại sao giải pháp An Hưu Phát Nghiệp cần cho các Doanh nghiệp?</h2>
+          </div>
+          </div>
+          <div class="why-list clearfix">
+            <div class="col-md-6">
+            <div class="w-item clearfix">
+              <div class="p-h1">
+                <img src="images/p-h1.png" alt="">
+              </div>
+              <div class="w-content">
+                <h4>
+                  Nhân viên – Phúc lợi tốt
+                </h4>
+                <p>
+                  <span class="hl">An tâm gắn bó với</span> doanh nghiệp vì được đảm bảo chế độ hưu trí vững chắc khi nghỉ hưu
+                </p>
+                <p>
+                  <span class="hl">Được bảo vệ toàn diện</span> trước rủi ro trong suốt thời gian làm việc tại Doanh nghiệp
+                </p>
+                <p>
+                  Có thể <span class="hl">chủ động đóng góp thêm</span> để gia tăng quyền lợi của bản thân
+                </p>
+              </div>
+
+            </div>
+            </div>
+            <div class="col-md-6">
+            <div class="w-item clearfix">
+              <div class="p-h2">
+                <img src="images/p-h2.png" alt="">
+              </div>
+              <div class="w-content">
+                <h4>
+                 Doanh nghiệp – Gắn kết bền
+                </h4>
+                <p>
+                  <span class="hl">Giải pháp phúc lợi</span> gia tăng sự gắn kết của nhân viên với Doanh nghiệp.
+                </p>
+                <p>
+                  Giải pháp phúc lợi <span class="hl">hấp dẫn thu hút tuyển dụng</span> nhân viên xuất sắc, nâng cao uy tín thương hiệu tuyển dụng trên thị trường
+                </p>
+                <p>
+                  <span class="hl">Chủ động xây dựng kế hoạch hưu trí</span> phù hợp với nhiều cấp bậc nhân viên khác nhau
+                </p>
+              </div>
+
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+
+      </div>
+    </div>
+  </div>
+
+  <?php include 'footer.php'; ?>
+  <script>
+    jQuery(document).ready(function($) {
+      $('body').removeClass('home');
+    });
+  </script>
